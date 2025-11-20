9592482f314d48f5082a0f345e7249fef8cfe27a
GwPhoenix98 <ionschimbator98@gmail.com>
2025-11-20 21:22:58 +0200

    Update FlightBooking.java
    Add getter and setter methods

--- src/FlightBooking.java
+++ src/FlightBooking.java
@@ -5,5 +5,43 @@ public class FlightBooking {
     private String seatClass;
     private boolean hasLuggage;
 
-    public FlightBooking()
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getSeatClass() {
+        return seatClass;
+    }
+
+    public void setSeatClass(String seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public boolean isHasLuggage() {
+        return hasLuggage;
+    }
+
+    public void setHasLuggage(boolean hasLuggage) {
+        this.hasLuggage = hasLuggage;
+    }
 }

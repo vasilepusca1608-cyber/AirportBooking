bde4903611d14a1030adae8d613de974eb6e136b
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-22 16:30:59 +0200

    Add Destinatia

--- src/SettingFlightBooking.java
+++ src/SettingFlightBooking.java
@@ -7,5 +7,17 @@ public class SettingFlightBooking {
         FlightBooking flightBooking = new FlightBooking();
 
         obScanner.close();
+
+        System.out.println("Introduceti numele pasagerului: ");
+        String name = obScanner.nextLine();
+
+        System.out.println("Alegiti destinatia: ");
+        System.out.println("1. Paris");
+        System.out.println("2. Roma");
+        System.out.println("3. Londra");
+        System.out.println("4. Barcelona");
+        System.out.println("Introduceti numarul destinatiei: ");
+
+
     }
 }

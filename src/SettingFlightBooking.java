3ed84e21b1b5045319cfe4488c0f42c40c07bb0f
Ion Schimbator <ionschimbator98@gmail.com>
2025-11-22 12:25:44 +0200

    Add class with psvm

---
+++ src/SettingFlightBooking.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SettingFlightBooking {
+    public static void main(String[] args) {
+
+        Scanner obScanner           = new Scanner(System.in);
+        FlightBooking flightBooking = new FlightBooking();
+
+        obScanner.close();
+    }
+}
